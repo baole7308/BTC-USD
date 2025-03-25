318a224104ba8b67f8fd4c714b7c2e4ebb2f7845
Computer Vision <baole7308@gmail.com>
2025-03-25 17:32:22 +0700

    update

--- BTC.py
+++ BTC.py
@@ -18,18 +18,14 @@ class dataBTC:
     def created_data(self):
         self.driver.get(self.url)
         sleep(4)
-        while True:
-            self.driver.execute_script("window.scrollBy(0, 500);")
-            sleep(0.5)
+        for _ in range(1):
             web = self.driver.page_source
             soup = BeautifulSoup(web,'html.parser')
-            getdate = soup.find('span',class_= 'C($linkColor) Fz(14px)').text
-            check = getdate.split(' - ')[0]
-            data1 = soup.find('table',class_ ='W(100%) M(0)')
+            data1 = soup.find('tbody')
             datas = data1.find_all('tr')
             for data in datas : 
-                self.date.append(data.span.text)
-                results = data.find_all('td',class_ ='Py(10px) Pstart(10px)')
+                self.date.append(data.td.text)
+                results = data.find_all('td',class_ ='yf-1jecxey')[1:]
                 for result in results:
                     self.temp.append(result.text)
                 if len(self.temp) == 6:
@@ -40,10 +36,6 @@ class dataBTC:
                     self.result5.append(self.temp[4])
                     self.result6.append(self.temp[5])
                 self.temp = []       
-            self.date = self.date[1:-1]
-            if any(date3 == check for date3 in self.date):
-                print('Done')
-                break
     def save_data_to_csv(self):
         df = pd.DataFrame()
         df ['Date'] = self.date  
@@ -55,7 +47,7 @@ class dataBTC:
         df ['Volume'] = self.result6
 
         df.sort_index(ascending=False,  inplace=True)
-        df.to_csv('E:\Python\Bitcoin\ETH.csv',index=False)
+        df.to_csv('E:\Python\Bitcoin\BTC.csv',index=False)
     def exit(self):
         self.driver.quit()
     
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from BTC import dataBTC
 from Train import model
 def main():
+    with open('E:\Python\Bitcoin\BTC.csv', mode="w", newline="", encoding="utf-8") as file:
+        pass
     data = dataBTC()
     data.created_data()
     data.save_data_to_csv()
@@ -8,8 +10,9 @@ def main():
     df = model('E:\Python\Bitcoin\BTC.csv')
     df.preprocess()
     df.build_model()
-    df.train_model(10000,256)
+    df.train_model(1000,32)
     print('Predict:', df.predict_and_format())
     df.plot_loss()
 if __name__ == "__main__":
     main()
+    
